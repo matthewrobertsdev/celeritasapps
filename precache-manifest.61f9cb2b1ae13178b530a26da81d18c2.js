028b2f486bf4414561aa62873a744b6a66b756d8
matthewrobertsdev <31932179+matthewrobertsdev@users.noreply.github.com>
2020-12-29 00:46:17 -0500

    Updates

---
+++ precache-manifest.61f9cb2b1ae13178b530a26da81d18c2.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab7231fead1a65494af2cc245fbf228",
+    "url": "/celeritasapps/index.html"
+  },
+  {
+    "revision": "16a60341681fb685a1b8",
+    "url": "/celeritasapps/static/css/2.1c164e0d.chunk.css"
+  },
+  {
+    "revision": "43955ae5c18883e19779",
+    "url": "/celeritasapps/static/css/main.629cd0db.chunk.css"
+  },
+  {
+    "revision": "16a60341681fb685a1b8",
+    "url": "/celeritasapps/static/js/2.38babb75.chunk.js"
+  },
+  {
+    "revision": "c6f2a8cff5bda75ce9d9d2e5dad3a2a7",
+    "url": "/celeritasapps/static/js/2.38babb75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43955ae5c18883e19779",
+    "url": "/celeritasapps/static/js/main.0c03129e.chunk.js"
+  },
+  {
+    "revision": "6b94cf092d43ee2ffea7",
+    "url": "/celeritasapps/static/js/runtime-main.dbebe948.js"
+  },
+  {
+    "revision": "82f56c2963a9849e5639ca403e8ea805",
+    "url": "/celeritasapps/static/media/AlarmSetUp.82f56c29.png"
+  },
+  {
+    "revision": "6ddff3539348acab0668ac405c23fc75",
+    "url": "/celeritasapps/static/media/AnalogClock.6ddff353.png"
+  },
+  {
+    "revision": "cbd5634c62fe48d4e3f3b22ce1440649",
+    "url": "/celeritasapps/static/media/BlackClockDark.cbd5634c.png"
+  },
+  {
+    "revision": "2ea0086c1ab8ef500b94405271abfed8",
+    "url": "/celeritasapps/static/media/BlueClockDark.2ea0086c.png"
+  },
+  {
+    "revision": "1f16a20bbb010f15fffc46d6abe14ce3",
+    "url": "/celeritasapps/static/media/BlueClockLight.1f16a20b.png"
+  },
+  {
+    "revision": "b185f22ee0ccd0b42b699c461eca618a",
+    "url": "/celeritasapps/static/media/BrownClockDark.b185f22e.png"
+  },
+  {
+    "revision": "48e058844e339677ce7f18535282b250",
+    "url": "/celeritasapps/static/media/BrownClockLight.48e05884.png"
+  },
+  {
+    "revision": "ea66460a7e426cd53d6a3537c96720fb",
+    "url": "/celeritasapps/static/media/ClockPurpleGrayBackground.ea66460a.png"
+  },
+  {
+    "revision": "e8ce5dc84baec886a95c1001d43c73d4",
+    "url": "/celeritasapps/static/media/ClockPurpleTranslucentBackground.e8ce5dc8.png"
+  },
+  {
+    "revision": "1e2302acae619d74eea9d08194bf7d90",
+    "url": "/celeritasapps/static/media/ClockShowHideDate.1e2302ac.png"
+  },
+  {
+    "revision": "8ab46bac6a4db5ba13277fb716c6548a",
+    "url": "/celeritasapps/static/media/ClockShowHideDate.8ab46bac.mp4"
+  },
+  {
+    "revision": "8f0c878dd8584404238897ac4f81d5a5",
+    "url": "/celeritasapps/static/media/ClockShowHideSeconds.8f0c878d.mp4"
+  },
+  {
+    "revision": "847fed77a629a438c77a6c529abad175",
+    "url": "/celeritasapps/static/media/CustomColorClockDark.847fed77.png"
+  },
+  {
+    "revision": "a43a98d3d385b6d4af74a63eb2da0ef6",
+    "url": "/celeritasapps/static/media/CustomColorClockLight.a43a98d3.png"
+  },
+  {
+    "revision": "26d86da37fcd0ee14ea6be7727f1cfae",
+    "url": "/celeritasapps/static/media/DockClock.26d86da3.mp4"
+  },
+  {
+    "revision": "604505aeb95c66807209b6b471e37983",
+    "url": "/celeritasapps/static/media/DockClock.604505ae.png"
+  },
+  {
+    "revision": "04dd53aa7ff507631c56a94d3003865e",
+    "url": "/celeritasapps/static/media/GrayClockDark.04dd53aa.png"
+  },
+  {
+    "revision": "af15a5bd481e8cff7cb9433ec454d6ad",
+    "url": "/celeritasapps/static/media/GrayClockLight.af15a5bd.png"
+  },
+  {
+    "revision": "0c78204e6c4f367ff87992679627be5f",
+    "url": "/celeritasapps/static/media/GreenClockDark.0c78204e.png"
+  },
+  {
+    "revision": "20bcb98c2b7c6a9e837f6174ea4a5d2c",
+    "url": "/celeritasapps/static/media/GreenClockLight.20bcb98c.png"
+  },
+  {
+    "revision": "b5f4a92609c3afdbea0275a751e3ff53",
+    "url": "/celeritasapps/static/media/LightTimer.b5f4a926.png"
+  },
+  {
+    "revision": "316ab25499f412a477d352c5955b79dd",
+    "url": "/celeritasapps/static/media/OrangeClockDark.316ab254.png"
+  },
+  {
+    "revision": "f2c41a930448a4a24ad1afeb5400f944",
+    "url": "/celeritasapps/static/media/OrangeClockLight.f2c41a93.png"
+  },
+  {
+    "revision": "0769d9f2c7b12cbb1a1bc05d839bdc1e",
+    "url": "/celeritasapps/static/media/PinkClockDark.0769d9f2.png"
+  },
+  {
+    "revision": "d3ba9f78e056a5fb49e7cef61a92fdff",
+    "url": "/celeritasapps/static/media/PinkClockLight.d3ba9f78.png"
+  },
+  {
+    "revision": "e0dfdc4f6e8687167acbfc0aca482b52",
+    "url": "/celeritasapps/static/media/PurpleClockDark.e0dfdc4f.png"
+  },
+  {
+    "revision": "df21da49f6e43b5a437e535686ab3c5b",
+    "url": "/celeritasapps/static/media/PurpleClockLight.df21da49.png"
+  },
+  {
+    "revision": "96f6548a2975fc5084b159e57d9e1563",
+    "url": "/celeritasapps/static/media/RedClockDark.96f6548a.png"
+  },
+  {
+    "revision": "cff58b383bb904f7dde35421add502b8",
+    "url": "/celeritasapps/static/media/RedClockLight.cff58b38.png"
+  },
+  {
+    "revision": "c985df73e63794f9ea05e7cb11878eca",
+    "url": "/celeritasapps/static/media/SystemDarkClockDark.c985df73.png"
+  },
+  {
+    "revision": "d3a5956f33d51668f5dc697dd726fa12",
+    "url": "/celeritasapps/static/media/TimerSetUp.d3a5956f.png"
+  },
+  {
+    "revision": "1729a424c52f4639db52f9f45e431261",
+    "url": "/celeritasapps/static/media/UseTimer.1729a424.png"
+  },
+  {
+    "revision": "6a04a803d273f2ec193e5d0102a4dd5e",
+    "url": "/celeritasapps/static/media/UseTimer.6a04a803.mp4"
+  },
+  {
+    "revision": "1a548ced33998cfb5f9428d0f84ad34a",
+    "url": "/celeritasapps/static/media/WhiteClockDark.1a548ced.png"
+  },
+  {
+    "revision": "ecb1a53fbac2b9f70903464a0feb7ae5",
+    "url": "/celeritasapps/static/media/WhiteClockLight.ecb1a53f.png"
+  },
+  {
+    "revision": "2800d2526603f98c09063ca1be207473",
+    "url": "/celeritasapps/static/media/YellowClockDark.2800d252.png"
+  },
+  {
+    "revision": "cefd28f003262ed6c7c96dfc4be95add",
+    "url": "/celeritasapps/static/media/YellowClockLight.cefd28f0.png"
+  }
+]);
