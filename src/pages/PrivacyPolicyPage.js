67575faf30605bc39bde85e94aec29e4a624c434
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2021-10-12 14:44:52 -0400

    Updated FAQ, privacy policy, and fixed small issue with selected-navigation-item css.

--- src/pages/PrivacyPolicyPage.js
+++ src/pages/PrivacyPolicyPage.js
@@ -11,20 +11,19 @@ const PrivacyPolicyPage = () => {
             <h1 className='main-h-1'>
                 Privacy Policies
                 </h1>
-                <h2 className='main-text'> 
-                Any changes to the privacy policies of Clock Suite and Contact Cards 
-                will be reflected here.
-            </h2>
                 <AppIconWithTitle image={ClockSuite256} alt='Clock Suite App Icon' 
                 name='Clock Suite Privacy Policy' id='clocksuiteprivacypolicy'/>
             <h2 className='main-text'>
-              Clock Suite does not collect data from its users, and does not connect to the internet.
+              Clock Suite does not collect data from its users, and does not connect to the internet.  
+              Any changes to the privacy policy of Clock Suite will be reflected here.
             </h2> 
             <AppIconWithTitle image={ContactCards256} alt='Contact Cards App Icon' 
                 name='Contact Cards Privacy Policy' id='contactcardsprivacypolicy'/>
             <h2 className='main-text'>
-                Contact Cards syncs data with its users' private iCloud storage, unless the user has 
-                turned iCloud off for it.  It connects to the internet to sync with iCloud.
+                Contact Cards syncs data with its user's private iCloud storage, unless the user has 
+                turned iCloud off for it.  The developer cannot access this data.  Contact Cards 
+                connects to the internet to sync with iCloud.  Any changes to the privacy policy 
+                of Contact Cards will be reflected here.
             </h2> 
             <br></br><br></br><br></br>
             </main>);
