c22d826d12910281097bce05c1f5fc10a1c0040d
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2022-02-09 22:26:06 -0500

    Added FAQ for why the text is dark in light mode.

--- src/pages/SpiffyClock_FAQ_Page.js
+++ src/pages/SpiffyClock_FAQ_Page.js
@@ -22,11 +22,20 @@ export default function ClockSuite_FAQ_Page() {
       name='FAQ for Spiffy Clock' id='spiffyclockfaq'/>
       <br/>
       <h2 className="text-align-center">
-        Question: Why does night mode not make the text dark?
+        Question: Why is night mode not making the text dark?
       </h2>
       <h3 className="main-margin">
         Answer: For night mode to have effect, you must be using dark mode.  If it is
         not darkening the text, you must not be using system-wide dark mode at that time.
+      </h3>
+      <br/>
+      <h2 className="text-align-center">
+        Question: Why is the text dark in light mode?
+      </h2>
+      <h3 className="main-margin">
+        Answer: This is the convention on iOS devices.  In light mode, the backgrounds 
+        are lighter and the text is darker.  This is particularly important for colors 
+        like light mode yellow on which light text wouldn't show up very well.
       </h3>
       <br/>
       <hr className='main-margin'/>
